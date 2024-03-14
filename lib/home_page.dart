3177f1525125f0c5aac9ee290fe1ba61e878163e
Usaid Asif <usaidasif12@gmail.com>
2024-03-14 17:00:06 +0500

    Display Time

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -8,30 +8,45 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  TimeOfDay _timeOfDay = const TimeOfDay(hour: 8, minute: 30);
+
   void _showTimePcker() {
     showTimePicker(
       context: context,
       initialTime: TimeOfDay.now(),
-    );
+    ).then((value) {
+      setState(() {
+        _timeOfDay = value!;
+      });
+    });
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Center(
-        child: MaterialButton(
-          onPressed: _showTimePcker,
-          color: Colors.blue,
-          child: Padding(
-            padding: const EdgeInsets.all(20),
-            child: const Text(
-              'PICK TIME',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 30,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Text(
+              _timeOfDay.format(context).toString(),
+              style: const TextStyle(fontSize: 30),
+            ),
+            MaterialButton(
+              onPressed: _showTimePcker,
+              color: Colors.blue,
+              child: const Padding(
+                padding: EdgeInsets.all(20),
+                child: Text(
+                  'PICK TIME',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 30,
+                  ),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
